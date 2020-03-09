7cc0313ae1346a2b4538f01b10c0139a09de07d4
Ken <kenlin@bigobject.io>
2020-03-09 18:51:51 +0800

    Add disk_du monitor via remote ssh command

--- docker-setup/python/linux_disk_du.py
+++ docker-setup/python/linux_disk_du.py
@@ -1,7 +1,7 @@
 import sys
 from kit import RemoteCmd
 
-# python3 ... HOST_NAME HOST_ADDRESS PATH ALRT_PCTG
+# python3 ... HOST_NAME HOST_ADDRESS PATH ALERT_AT
 NAGIOS_OK = 0
 NAGIOS_WARNING = 1
 NAGIOS_CRITICAL = 2
@@ -19,27 +19,27 @@ def main():
     host_name = args[1]
     host_address = args[2]
     path = args[3]
-    alert_pctg = int(args[4])
-    remote_cmd = "df -h {!s}".format(path)
+    alert_at = float(args[4])
+    remote_cmd = "du -sm {!s}".format(path)
     
     # send remote command via SSH
     res = RemoteCmd.commit(host_name, host_address, remote_cmd)
     
     # message stack
     msg_stack = res.split('\n')
-    if len(msg_stack)<=3 :
+    if len(msg_stack)<=2 :
         print("ssh response malformat {!s}".format(msg_stack))
         sys.exit(NAGIOS_WARNING)
     line = msg_stack[-2]
     try:
-        [device, total, used, free, used_r, mnt] = line.split()
+        [used_mb, path] = line.split()
+        used_mb = float(used_mb)
     except Exception as e:
         print("remote command response malformat {!s}".format(msg_stack))
         sys.exit(NAGIOS_WARNING)
         
-    used_pctg = int(used_r[:-1])
-    print("{!s} used {!s}({!s}), alert at {!s}%".format( mnt, used_r, free, alert_pctg ))
-    if used_pctg >= alert_pctg:
+    print( "{!s} has {!s} MB (alert at: {!s} MB)".format( path, used_mb, alert_at ) )
+    if used_mb >= alert_at:
         sys.exit(NAGIOS_WARNING)
         
     sys.exit(NAGIOS_OK)
