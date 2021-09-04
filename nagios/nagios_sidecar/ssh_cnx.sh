51b028f48c9e6bcec1ccc712488054540390b835
ken.lin <ken.lin@bigobject.io>
2021-09-04 18:41:45 +0800

    update ssh_cnx for sidecar

--- nagios/nagios_sidecar/ssh_cnx.sh
+++ nagios/nagios_sidecar/ssh_cnx.sh
@@ -3,4 +3,4 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
 # execute script
-/usr/local/nagios/libexec/pybox/bin/python /usr/local/nagios/libexec/python/create_ssh_template.py
+/usr/local/nagios/libexec/pybox/bin/python /usr/local/nagios/libexec/python/sidecar_ssh_cnx.py
