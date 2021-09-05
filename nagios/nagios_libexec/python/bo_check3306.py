f2bdf621e80152bb99dac763d8f27c9d5e9c3bf1
ken.lin <ken.lin@bigobject.io>
2021-09-05 12:03:46 +0800

    bo_3306 with ciphered password

--- nagios/nagios_libexec/python/bo_check3306.py
+++ nagios/nagios_libexec/python/bo_check3306.py
@@ -1,5 +1,7 @@
 import sys
+import click
 from kit import BigObject
+from kit import Fernet
 
 # python3 ... HOST PORT
 NAGIOS_OK = 0
@@ -7,30 +9,32 @@ NAGIOS_WARNING = 1
 NAGIOS_CRITICAL = 2
 NAGIOS_UNKNOWN = 3
 
-def main():
-    # verify input
-    args = sys.argv
+@click.command(help='check BO license time')
+@click.argument('host')
+@click.argument('port')
+@click.argument('db')
+@click.option('--user', default='root')
+@click.option('--password', default='')
+def main(host, port, db, user, password):
+    """ Verify BO liveness via sql connection query """
+    # decipher password
+    if len(password) != 0:
+        fernet = Fernet.getOptEnv()
+        password = fernet.decrypt(password)
 
-    if len(args) < 3:
-        raise Exception('number of arguments not enough')
-
-    host = args[1]
-    port = int(args[2])
-    db = args[3]
-    
     # setup connection
-    bo_client = BigObject.connect(host, port, db)
+    bo_client = BigObject.connect(host, port, db, user, password)
     result = bo_client.query("SELECT 1 as result")
-    
+
     # get result and verify
     row = result.fetchone()
     if row.get('result',0) != 1:
         print('SELECT 1 error')
         sys.exit(NAGIOS_WARNING)
-    
+
     print('OK')
     sys.exit(NAGIOS_OK)
-    
+
 # : def main
 
 if __name__ == '__main__':
@@ -39,4 +43,3 @@ if __name__ == '__main__':
     except Exception as e:
         print("exception caught as follows:{!s}".format(e))
         sys.exit(NAGIOS_UNKNOWN)
-    
