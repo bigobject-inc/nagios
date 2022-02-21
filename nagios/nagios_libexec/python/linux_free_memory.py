84c9cc9d27525f294cdf8ae0075df97ecda6039f
ken.lin <ken.lin@bigobject.io>
2022-02-21 09:34:25 +0800

    upgrade free memory

--- nagios/nagios_libexec/python/linux_free_memory.py
+++ nagios/nagios_libexec/python/linux_free_memory.py
@@ -7,7 +7,6 @@ NAGIOS_WARNING = 1
 NAGIOS_CRITICAL = 2
 NAGIOS_UNKNOWN = 3
 
-
 def main():
     # verify input
     args = sys.argv
@@ -16,36 +15,39 @@ def main():
         raise Exception('number of arguments not enough')
 
     # setup parameter
+    startline = "====== stats below ======"
     host_name = args[1]
     host_address = args[2]
     alert_at = float(args[3])
-    remote_cmd = "free -m "
-    
+    remote_cmd = f"echo '{startline}' && ps -eo pid,cmd,%mem,%cpu --sort=-%mem | head -10 && free -m "
+
     # send remote command via SSH
     res = RemoteCmd.commit(host_name, host_address, remote_cmd)
-    
+
     # message stack
     msg_stack = res.split('\n')
     if len(msg_stack)<=2 :
         print("ssh connection error: {!s}".format(msg_stack))
         sys.exit(NAGIOS_WARNING)
-    line  = msg_stack[-3]
+
     try:
+        start =msg_stack.index(startline)
+        line  = msg_stack[-3]
         [_, total, used, free, _, _, _] = line.split()
         total = float(total)
         used = float(used)
     except Exception as e:
         print("remote command response malformat {!s}".format(msg_stack))
         sys.exit(NAGIOS_WARNING)
-    
+
     used_r = int(100*used/total)
     print( "total: {!s}MB, used: {!s}MB({!s}%), alert if >={!s}%".format( total, used, used_r, alert_at ) )
+    for line in msg_stack[start:-4]:
+        print(line)
     if used_r >= alert_at:
         sys.exit(NAGIOS_WARNING)
-        
+
     sys.exit(NAGIOS_OK)
-    
-    
 # : def main
 
 if __name__ == '__main__':
@@ -54,4 +56,3 @@ if __name__ == '__main__':
     except Exception as e:
         print("exception caught as follows:{!s}".format(e))
         sys.exit(NAGIOS_UNKNOWN)
-    
